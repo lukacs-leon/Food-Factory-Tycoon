475f17b9d66683d97492e7a3f8e6583f8ff486e0
lukacs-leon <160414449+lukacs-leon@users.noreply.github.com>
2025-07-01 14:25:31 +0000

    safety save

--- tester.py
+++ tester.py
@@ -13,28 +13,40 @@ class Recipe:
         self.outputs = outputs
         self.machine = machine
         self.time = time
+    def get_atributes(self):
+        atributes = {
+            "name": self.name,
+            "inputs": self.inputs,
+            "outputs": self.outputs,
+            "machine": self.machine,
+            "time": self.time
+        }
+        return
 class Machine:
-    def __init__(self, name):
+    def __init__(self, name, machine_type):
+        self.machine_type = machine_type
         self.name = name
         self.current_recipe = None
-        self.progress = 0  # gyártás állapota
+        self.progress = 0
 
     def start_recipe(self, recipe):
-        self.current_recipe = recipe
-        self.progress = 0
+        if recipe.machine == self.machine_type:
+            self.current_recipe = recipe
+            self.progress = 0
+        else:
+            print(f"error! couldn't make this recipe ({self.current_recipe.name}) with this machine: {self.name} (type: {self.machine_type})")
 
     def tick(self):
         if self.current_recipe is not None:
             self.progress += 1
             if self.progress >= self.current_recipe.time:
-                # kész a termék
-                print(f"{self.name} elkészült a {self.current_recipe.name}")
+                print(f"{self.name} had made the {self.current_recipe.name}")
                 self.current_recipe = None
                 self.progress = 0
 if __name__ == "__main__":
     materials = {}
     def create_materials(materials_list: list):
-        for mat_name in materials_list:  # itt a bemeneti listán megyünk végig!
+        for mat_name in materials_list:
             material_obj = RawMaterial(mat_name)
             materials[mat_name] = material_obj
         print(materials)
@@ -49,5 +61,16 @@ if __name__ == "__main__":
             reciptes_object = Recipe(name, inputs, outputs, machine, time)
             reciptes[name] = reciptes_object
         print(reciptes)
+    machins = {}
+    def create_machine(name: str):
+        machine = Machine(name, "oven")
+        machins[name] = machine
+
     create_materials(["egg", "water", "wheat"])
     create_reciptes([{"name": "bread", "inputs": ["egg", "water", "wheat"], "outputs": "bread", "machine": "oven", "time": 2}])
+    create_machine("oven1")
+    oven1 = machins["oven1"]
+    oven1.start_recipe(reciptes["bread"])
+    for key, value in machins:
+        value.tick()
+        print(f"{key} has ticked")
