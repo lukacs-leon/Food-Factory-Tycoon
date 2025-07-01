12a3c63efade1d10baf410efdd6d94f3513ef086
lukacs-leon <160414449+lukacs-leon@users.noreply.github.com>
2025-07-01 13:40:31 +0000

    added the recipe's test and tested sucsessful

--- tester.py
+++ tester.py
@@ -2,7 +2,7 @@ class RawMaterial:
     def __init__(self, name):
         self.name = name
 class Recipe:
-    def __init__(self, name, inputs, outputs, time):
+    def __init__(self, name, inputs, outputs, machine, time):
         """
         inputs: dict {RawMaterial: mennyiség}
         outputs: dict {RawMaterial: mennyiség}
@@ -11,6 +11,7 @@ class Recipe:
         self.name = name
         self.inputs = inputs
         self.outputs = outputs
+        self.machine = machine
         self.time = time
 class Machine:
     def __init__(self, name):
@@ -37,4 +38,16 @@ if __name__ == "__main__":
             material_obj = RawMaterial(mat_name)
             materials[mat_name] = material_obj
         print(materials)
+    reciptes = {}
+    def create_reciptes(reciptes_list: list):
+        for i in reciptes_list:
+            name = i["name"]
+            inputs = i["inputs"]
+            outputs = i["outputs"]
+            machine = i["machine"]
+            time = i["time"]
+            reciptes_object = Recipe(name, inputs, outputs, machine, time)
+            reciptes[name] = reciptes_object
+        print(reciptes)
     create_materials(["egg", "water", "wheat"])
+    create_reciptes([{"name": "bread", "inputs": ["egg", "water", "wheat"], "outputs": "bread", "machine": "oven", "time": 2}])
